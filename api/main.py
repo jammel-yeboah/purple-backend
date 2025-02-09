bf9442006bc2eeba8f4864ed6e63d30a149b2d0d
jammely-uva <jammelyeboah@gmail.com>
2025-02-09 23:55:42 +0100

    update port number

--- api/main.py
+++ api/main.py
@@ -1,6 +1,10 @@
 # from fastapi import FastAPI
 from api.endpoints import router  # Import your endpoint router
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 app.include_router(router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=80)
