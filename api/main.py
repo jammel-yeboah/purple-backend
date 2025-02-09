77dfadb5a26ff95efe227c61b8632f1b4bbb29c8
jammely-uva <jammelyeboah@gmail.com>
2025-02-10 00:34:16 +0100

    revert main.py

--- api/main.py
+++ api/main.py
@@ -2,19 +2,9 @@
 from api.endpoints import router  # Import your endpoint router
 from fastapi import FastAPI
 import uvicorn
-import logging
 
 app = FastAPI()
 app.include_router(router)
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-@app.on_event("startup")
-async def startup_event():
-    logger.info("Application startup")
-    for route in app.routes:
-        logger.info(f"Route: {route.path} - {route.name}")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=80)
